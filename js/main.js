20b97c0e8adf175a7d69250c54b9d3980fe1862d
sveinnmani <sveinnmani@hotmail.com>
2017-11-14 14:29:46 +0000

    Set gögn um skjalfta í fylki
    Næ í gögn úr API og set þau inn í fylki.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+"use strict";
+var skjalftar = [];
+
+$.ajax({
+	'url': 'http://apis.is/earthquake/is',
+	'type': 'GET',
+	'dataType': 'JSON',
+	'success': function(data)	{
+		for (var i = 0; i < data.results.length; i++) {
+			skjalftar.push({
+				lat: data.results[i].latitude,
+				lng: data.results[i].longitude,
+				richter: data.results[i].size
+			});
+		}
+		console.log(skjalftar);
+	}
+});
+
+
+//Þetta er fall sem býr til kortið af Íslandi
 function initMap()	{
 	var stadsetning = {lat: 65, lng: -19};
 	var kort = new google.maps.Map(document.getElementById('island'), {
