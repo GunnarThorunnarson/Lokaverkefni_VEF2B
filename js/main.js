a43dfaa02d022ac14cb573077341d07e17f503c8
sveinnmani <sveinnmani@hotmail.com>
2017-11-28 14:52:55 +0000

    Röðun er í vinnslu
    Er að vinna í að flokka hringina.

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ function initMap() {
 				});
 			}		
 			lengd = skjalftar.length;
-			console.log(skjalftar[3].lat);
 			var stadsetning = {lat: 65, lng: -19};
 			var kort = new google.maps.Map(document.getElementById('island'), {
 				zoom: 6,
@@ -28,37 +27,49 @@ function initMap() {
 			});
 			(function() {
 				function init() {
+					var rows = [],
+						$min = $('#value-min'),
+						$max = $('#value-max'),
+						$table = $('#rates');
 					for(var x = 0; x < lengd; x++) {
 						var skjalftaHringur = new google.maps.Circle({
 							strokeColor: '#FF0000',
-           					strokeOpacity: 0.8,
-           					strokeWeight: 2,
+           					strokeOpacity: 0.5,
+           					strokeWeight: 5,
            					fillColor: '#FF0000',
         	   				fillOpacity: 0.35,
            					map: kort,
            					center: {lat: skjalftar[x].lat, lng: skjalftar[x].lng},
            					radius: skjalftar[x].richter * 10000
 						});
+						
 					}
-					var rows = [],
-						$min = $('#value-min'),
-						$max = $('#value-max'),
-						$table = $('#rates');
+					
+					for (var y = 0; y < lengd; y++) {
+						update(skjalftaHringur, skjalftar[y].richter, $min, $max.val());
+					}
+					
 					$('#slider').noUiSlider({
 						range: [0, 11],
-						start: [0, 11],
+						start: [5, 11],
 						handles: 2,
 						margin: 1,
 						connect: true,
 						serialization: {to: [$min, $max], resolution: 1}
 					});
-					function update(min, max)	{
-						skjalftar.forEach(function(stakur)	{
-							console.log(stakur);
-							console.log($min.val(), $max.val());
-						});
+					function update(circle, skali, min, max)	{
+						if(skali >= min && skali <= max) {
+							circle.setMap(kort);
+							//skali = 1;
+						}
+						else {
+							circle.setMap(null);
+						}
+						var name = document.getElementById("value-max").value;
+						console.log(name);
 					}
-					update(9, 10);
+					
+					
 				}
 				$(init);
 			}());
