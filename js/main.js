53b7ae2cae5478e9091bf70a32fa8f9b184cc1d7
sveinnmani <sveinnmani@hotmail.com>
2017-11-30 12:42:16 +0000

    Bætti við ýmsu
    Bætti Moment.js við til að formatta tímann, tölurnar fyrir slider eru núna virkar og bætti við infowindow þegar maður smellir á jarðskjálfta.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 "use strict";
 var skjalftar = [];
 var lengd = 0;
-
+var skjaftaFylki = [];
+var dt = '2017-02-17T22:32:25.000Z';
+console.log(new Date(dt).toLocaleString('en-CA', { hour12:true }));
 
 function initMap() {
 	//Næ í gögn frá apis.is
@@ -15,7 +17,9 @@ function initMap() {
 				skjalftar.push({
 					lat: data.results[i].latitude,
 					lng: data.results[i].longitude,
-					richter: data.results[i].size
+					richter: data.results[i].size,
+					hvar: data.results[i].humanReadableLocation,
+					hvenaer: data.results[i].timestamp
 				});
 			}		
 			lengd = skjalftar.length;
@@ -27,10 +31,10 @@ function initMap() {
 			});
 			(function() {
 				function init() {
-					var rows = [],
+					/*var rows = [],
 						$min = $('#value-min'),
 						$max = $('#value-max'),
-						$table = $('#rates');
+						$table = $('#rates');*/
 					for(var x = 0; x < lengd; x++) {
 						var skjalftaHringur = new google.maps.Circle({
 							strokeColor: '#FF0000',
@@ -42,33 +46,70 @@ function initMap() {
            					center: {lat: skjalftar[x].lat, lng: skjalftar[x].lng},
            					radius: skjalftar[x].richter * 10000
 						});
+						skjaftaFylki.push(skjalftaHringur);
+						synaSkala(skjalftaHringur, skjalftar[x].richter.toString(), {lat: skjalftar[x].lat, lng: skjalftar[x].lng}, skjalftar[x].hvar, skjalftar[x].hvenaer);						
+					}
+					uppfaera(skjaftaFylki, skjalftar, 1, 10);					
+					function deila(str) {
+						var parts = str.split('T');
+						parts = str.split('.');
+						return parts[0]
+					}			
+					function uppfaera(circle, skali, min, max)	{
+						var slider = document.getElementById('slider');
+
+						noUiSlider.create(slider, {
+							start: [0, 11],
+							connect: true,
+							margin: 1,
+							range: {
+								'min': 0, 
+								'max': 10
+							}
+						});
+						var inputNumber1 = document.getElementById('value-min');
+						var inputNumber2 = document.getElementById('value-max');
 						
-					}
-					
-					for (var y = 0; y < lengd; y++) {
-						update(skjalftaHringur, skjalftar[y].richter, $min, $max.val());
-					}
-					
-					$('#slider').noUiSlider({
-						range: [0, 11],
-						start: [5, 11],
-						handles: 2,
-						margin: 1,
-						connect: true,
-						serialization: {to: [$min, $max], resolution: 1}
-					});
-					function update(circle, skali, min, max)	{
-						if(skali >= min && skali <= max) {
-							circle.setMap(kort);
-							//skali = 1;
-						}
-						else {
-							circle.setMap(null);
-						}
-						var name = document.getElementById("value-max").value;
-						console.log(name);
-					}
-					
+						console.log(skjalftaHringur);
+						slider.noUiSlider.on('update', function(values, handle) {
+							var value = values[handle];
+							if(handle){
+								inputNumber2.value = value;
+							} else {
+								inputNumber1.value = value;
+							}
+							for (var y = 0; y < lengd; y++) {
+								console.log(skali[y].richter);
+									if(skali[y].richter >= values[0] && skali[y].richter <= values[1]){
+										//circle.setMap(kort);
+										circle[y].setMap(kort);
+									}
+									else{
+										circle[y].setMap(null);
+									}
+								}
+						});	
+						inputNumber1.addEventListener('change', function() {
+							slider.noUiSlider.set([null, this.value]);
+						});					
+						inputNumber2.addEventListener('change', function() {
+							slider.noUiSlider.set([this.value, null]);
+						});
+					}	
+
+					function synaSkala(circle, skali, local, hvar, hvenaer)	{
+						console.log(hvenaer);
+						var infowindow = new google.maps.InfoWindow({							
+							content: '<p>Staðsetning:  ' + hvar + '</p>' +
+									 '<p>Stærð: ' + skali + '</p>' +
+									 '<p>Tími: ' + moment(hvenaer).format('HH:MM') + '</p>' +
+									 '<p>Dagsetning: ' + moment(hvenaer).format('DD/MM/YYYY') + '</p>',
+							position: local
+						});
+						circle.addListener('click', function() {
+ 							infowindow.open(circle.get('kort'), circle);
+ 						}); 						
+					}		
 					
 				}
 				$(init);
@@ -76,12 +117,3 @@ function initMap() {
 		}		
 	});		
 }
-  
-/* Þetta var bara eitthvað test
-function synaSkala(circle, skali, local)	{
-	var infowindow = new google.maps.InfoWindow({
-		content: skali,
-		position: local
-	});
-}*/
-
